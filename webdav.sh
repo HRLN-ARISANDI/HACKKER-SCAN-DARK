fb3d82feeeb88b01e188274933252998cb9d6bce
ARISANDI25 <59158593+ARISANDI25@users.noreply.github.com>
2019-12-23 13:40:46 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,74 @@
+
+clear
+echo "╲╲╲╲╲╲╲ ╱╱╱╱╱╭╯
+╲╭━┻┻┻┻━╮╱╱╱┏┓╱
+╭┫╭━╮╭━┓┣╮╱╱┣┫╱
+┃┃┃▆┃┃▋┃┃┃╱╭╯╰╮
+╰┫┗━╭╮━╯┣╯╲┃╭╮┃
+╱┃╰┳┳━━╯┃╲╲┃╳╳┃
+╱┃╲╰╯╱╱╱┃╲╲┃╰╯┃
+╱╰━┳━━┳━╯╲╲┗━━┛" | lolcat
+cowsay -f eyes 'ARISANDI' | lolcat
+figlet -f slant "ARISANDI" | lolcat
+
+
+echo "Tools By Hrln.Arisandi" | lolcat
+echo "Tools installer" | lolcat
+echo "Team : HSD(HACKKER SCAN DARK)" | lolcat
+echo "====================" | lolcat
+echo "1.Install semua tools nya"
+echo "===================="
+echo "2.hack facebook"
+echo "==================="
+echo "3.hack whatsapp"
+echo "==================="
+echo "4.kirim virus"
+echo "==================="
+echo "5.installer HSD"
+echo "==================="
+echo "6.installer SCAN DARK"
+echo "==================="
+read -p "r00t@SCANDARK~# " pillif [ $pill = 1 ]
+if [ $pill = 2 ]
+then
+clear
+echo "Tunggu gaiz sabar....1!1!1"
+echo
+sleel 1
+pkg install git
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+ls
+python2 SETAN.py
+fi
+
+
+
+if [ $pill = 3 ]
+then
+clear
+sleel 1
+pkg install git
+git clone htrps://github.com/AndriGanz/whatshack
+cd whatshack
+ls
+sh whatshack.sh
+fi
+
+
+
+if [ $pill = 4 ]
+then
+clear
+sleel 1
+pkg install git
+git clone https://github.com/justahackers/perusak
+cd perusak
+ls
+python2 perusak.py
+fi
+then
+
+
+
+figlet -r WELLCOME TO TERMUX" | lolcat
